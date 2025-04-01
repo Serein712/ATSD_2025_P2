7a933a038fe41a986fddc119234a1c0eb3efe6d0
Serein712 <oleg.sumin51@gmail.com>
2025-04-01 14:29:21 +0200

    User profile page added
    Added user page with user data, also there is a link from /registered for each user in the list

--- p2-todolist-app/src/test/java/todolist/controller/RegisteredPageTest.java
+++ p2-todolist-app/src/test/java/todolist/controller/RegisteredPageTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -18,8 +19,17 @@ public class RegisteredPageTest {
     private MockMvc mockMvc;
 
     @Test
-    public void getAboutDevuelveNombreAplicacion() throws Exception {
+    public void getRegisteredPage() throws Exception {
         this.mockMvc.perform(get("/registered"))
                 .andExpect(content().string(containsString("Usuarios")));
     }
+
+    @Test
+    public void getRegisteredUserPage() throws Exception {
+        this.mockMvc.perform(get("/registered/1"))
+                .andExpect(content().string(allOf(
+                        containsString("Richard Stallman"),
+                        containsString("richard"))));
+    }
+
 }
