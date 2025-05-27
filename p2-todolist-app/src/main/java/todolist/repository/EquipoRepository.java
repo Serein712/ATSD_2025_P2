648639cb2938521609074dea40c38eac885dbf96
Serein712 <oleg.sumin51@gmail.com>
2025-05-27 15:29:09 +0200

    Fifth test - List of teams

--- p2-todolist-app/src/main/java/todolist/repository/EquipoRepository.java
+++ p2-todolist-app/src/main/java/todolist/repository/EquipoRepository.java
@@ -2,9 +2,10 @@ package todolist.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import todolist.model.Equipo;
-
+import java.util.List;
 import java.util.Optional;
 
 public interface EquipoRepository extends CrudRepository<Equipo, Long>{
     Optional<Equipo> findByNombre(String s);
+    public List<Equipo> findAll();
 }
