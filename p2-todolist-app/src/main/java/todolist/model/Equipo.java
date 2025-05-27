e831300b9b5831633f0adc070401491eb17cf499
Serein712 <oleg.sumin51@gmail.com>
2025-05-27 12:53:52 +0200

    fix Third test
    Added equipo equals and hashCode methods

--- p2-todolist-app/src/main/java/todolist/model/Equipo.java
+++ p2-todolist-app/src/main/java/todolist/model/Equipo.java
@@ -44,5 +44,20 @@ public class Equipo implements Serializable {
         this.nombre = titulo;
     }
 
-    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        todolist.model.Equipo equipo = (todolist.model.Equipo) o;
+        if (id != null && equipo.id != null)
+            // Si tenemos los ID, comparamos por ID
+            return Objects.equals(id, equipo.id);
+        // si no comparamos por campos obligatorios
+        return nombre.equals(equipo.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre);
+    }
 }
