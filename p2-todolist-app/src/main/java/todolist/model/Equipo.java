9dbfbb197f0efcb3674f1bc069942645cc6b0207
Serein712 <oleg.sumin51@gmail.com>
2025-05-27 13:45:20 +0200

    Fourth test - Many-to-many relationship between Equipos and Usuarios

--- p2-todolist-app/src/main/java/todolist/model/Equipo.java
+++ p2-todolist-app/src/main/java/todolist/model/Equipo.java
@@ -4,6 +4,8 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Objects;
+import java.util.Set;
+import java.util.HashSet;
 
 @Entity
 @Table(name = "equipos")
@@ -17,7 +19,14 @@ public class Equipo implements Serializable {
     @NotNull
     private String nombre;
 
-    // Constructor vacío necesario para JPA/Hibernate.
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "equipo_usuario",
+            joinColumns = { @JoinColumn(name = "fk_equipo") },
+            inverseJoinColumns = {@JoinColumn(name = "fk_usuario")})
+
+    Set<Usuario> usuarios = new HashSet<>();
+
+
     // No debe usarse desde la aplicación.
     public Equipo() {}
 
@@ -43,6 +52,20 @@ public class Equipo implements Serializable {
     public void setNombre(String titulo) {
         this.nombre = titulo;
     }
+
+    public Set<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void addUsuario(Usuario usuario) {
+        // Hay que actualiar ambas colecciones, porque
+        // JPA/Hibernate no lo hace automáticamente
+        this.getUsuarios().add(usuario);
+        usuario.getEquipos().add(this);
+    }
+
+    // equals para el tercer test
+    // que aparece en la prácitca
 
     @Override
     public boolean equals(Object o) {
